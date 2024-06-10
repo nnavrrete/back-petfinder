316d951a57a2e327c36bb73c7efaa232fbbdb67a
Nicolas <nnavarrete@utem.cl>
2024-06-09 22:37:59 -0400

    refactorizacion rutas y nombres de archivos

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,34 @@ const Routes = (app) => {
   // Ruta para crear un nuevo dueño
   app.post('/dueno', async (req, res) => {
     try {
-      const { nombre, correo, telefono, direccion, id_redsocial } = req.body;
-      if (!id_redsocial  || !nombre || !correo || !telefono || !direccion) {
+      const { nombre, correo, telefono, dirección, id_redsocial } = req.body;
+      console.log('Received data:', req.body); // Log received data
+      
+      if (!id_redsocial || !nombre || !correo || !telefono || !dirección) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios' });
       }
-      const query = 'INSERT INTO dueno (nombre, correo, telefono, dirección, id_redsocial) VALUES ($1, $2, $3, $4, $5)';
-      const values = [nombre, correo, telefono, direccion, id_redsocial];
-      const result = await pool.query(query, values);
+
+      // Check if the user already exists
+      const checkQuery = 'SELECT * FROM dueno WHERE correo = $1';
+      const checkResult = await pool.query(checkQuery, [correo]);
+
+      if (checkResult.rows.length > 0) {
+        // User already exists
+        return res.status(409).json({ message: 'El usuario ya existe' });
+      }
+
+      // Insert new user if not exists
+      const insertQuery = 'INSERT INTO dueno (nombre, correo, telefono, dirección, id_redsocial) VALUES ($1, $2, $3, $4, $5)';
+      const insertValues = [nombre, correo, telefono, dirección, id_redsocial];
+      const result = await pool.query(insertQuery, insertValues);
+
       res.status(201).json({ message: 'Registro creado exitosamente', result });
     } catch (error) {
-      console.error(error);
+      console.error('Error occurred during query:', error); // Log detailed error
       res.status(500).json({ message: 'Error interno del servidor' });
     }
   });
-
+  
   // Ruta para obtener los dueños por su id_redsocial
   app.get('/:id_redsocial', async (req, res) => {
     try {
@@ -30,6 +44,13 @@ const Routes = (app) => {
       res.status(500).json({ message: 'Error interno del servidor' });
     }
   });
+
+  app.get('/' , (req, res) => {
+    res.send('Hola mundo');
+  });
+
 };
+
+
 
 module.exports = { Routes };
